7dcfa777b55536949d45e885f2c7c27e1bd3c224
APJohns <mail@ashpjohns.com>
2023-05-16 16:40:07 -0400

    add script to update readme version on build

---
+++ updateVersions.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const package = JSON.parse(
+  fs.readFileSync("package.json", {
+    encoding: "utf8",
+  })
+);
+
+const readme = fs.readFileSync("README.md", {
+  encoding: "utf8",
+});
+
+fs.writeFileSync(
+  "README.md",
+  readme.replaceAll(
+    /anatomy-tokens@\d+\.\d+\.\d+/g,
+    `anatomy-tokens@${package.version}`
+  )
+);
